ea8e091e78cbe46a8de2e0eea4ba86101b3e1c38
yunasim <ynsim@solidit.co.kr>
2023-06-24 00:25:19 +0900

    #✨feature: 1차 어댑터 디자인패턴 구현

---
+++ ConsoleApp1/AdapterPattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific Request";
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        private readonly Adaptee m_adaptee;
+
+        public Adapter(Adaptee adaptee) 
+        { 
+            m_adaptee = adaptee;  
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this.m_adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+            Console.WriteLine(target.GetRequest());
+        }
+    }
+}
