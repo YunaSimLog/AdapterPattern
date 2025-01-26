50229ad6525cb0a55267667d897b5ba9dc862c28
yunasim <ynsim@solidit.co.kr>
2025-01-26 16:51:38 +0900

    #😎update: 서비스 클래스 Program.cs 위치로 이동

--- AdapterPattern_2/ObjectAdaptor.cs
+++ AdapterPattern_2/ObjectAdaptor.cs
@@ -6,17 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdapterPattern_2
 {
-    /// <summary>
-    /// Adaptee : 클라이언트에서 사용하고 싶은 기존의 서비스 (하지만 호한이 안되서 바로 사용 불가능)
-    /// </summary>
-    class Service
-    {
-        // 서비스에 사용할 메소드
-        public void SpecificMethod(int specialData)
-        {
-            Console.WriteLine($"기존 서비스 기능 호출 {specialData}");
-        }
-    }
+
 
     /// <summary>
     /// Client Interface : 클라이언트가 접근해서 사용할 고수준의 어댑터 모듈
--- AdapterPattern_2/Program.cs
+++ AdapterPattern_2/Program.cs
@@ -19,4 +19,16 @@ namespace AdapterPattern_2
             // -----------------------------------------------------------------------
         }
     }
+
+    /// <summary>
+    /// Adaptee : 클라이언트에서 사용하고 싶은 기존의 서비스 (하지만 호한이 안되서 바로 사용 불가능)
+    /// </summary>
+    class Service
+    {
+        // 서비스에 사용할 메소드
+        public void SpecificMethod(int specialData)
+        {
+            Console.WriteLine($"기존 서비스 기능 호출 {specialData}");
+        }
+    }
 }
